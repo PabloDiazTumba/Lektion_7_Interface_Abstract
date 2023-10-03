958bde8a132ca4e1001a3f1254208e932e0957f0
PabloDiazTumba <pablodiaz147@gmail.com>
2023-10-03 10:00:41 +0200

    First commit

---
+++ src/com/Pablo/interfaceAbstract/Animal.java
@@ -0,0 +1,13 @@
+package com.Pablo.interfaceAbstract;
+
+public interface Animal {
+
+    // TODO - Give relevant names
+    // TODO - Implement Inheritance
+    
+
+    void myAbstractMethod ();
+
+    default void test() {}
+
+}
